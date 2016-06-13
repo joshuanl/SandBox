01198b2521519786b8294258515db805634da8b4
joshuanl <joshuanl@usc.edu>
2016-06-12 19:51:23 -0700

    added java file

---
+++ gitpractice.java
@@ -0,0 +1,16 @@
+package default;
+
+public class SandBox{
+	public static void main(String[] args) {
+
+		int num1 = 10;
+		int num2 = 20;		
+
+
+		System.out.println(num1);
+		System.out.println(num2);
+		num2 = Math.sqrt(num1);
+		System.out.println("welp");
+	}//end of main
+}//end of class
+
